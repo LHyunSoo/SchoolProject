86135430775d45936297d88742266afb6d577c44
LHyunSoo <s2018w36@e-mirim.hs.kr>
2019-04-04 08:12:59 +0900

    WebProject2018

---
+++ js/menu.js
@@ -0,0 +1,26 @@
+
+function go_main(){
+    window.open("index.html");
+}
+
+function smallImg(x) {
+    x.style.width = "390px";
+    x.style.height = "310px";
+}
+function normalImg(x) {
+    x.style.width = "400px"; 
+    x.style.height = "320px";
+}
+
+function EjrqhRdl(){
+    window.open("EjrqhRdl.html");
+}
+function tnseo(){
+    window.open("tnseo.html");
+}
+function dhepd(){
+    window.open("dhepd.html");
+} 
+function xnlrla(){
+    window.open("xnlrla.html");
+}
